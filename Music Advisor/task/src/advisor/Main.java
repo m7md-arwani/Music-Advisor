9bfefe72b31e78be58a41aca59524c0c9a307a90
m7md-arwani <moh.3ara@hotmail.com>
2021-09-28 08:50:06 +0300

    Enhancing the app for later use. Stage(2/5)

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -1,55 +1,57 @@
 package advisor;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
+
+    public static String url = "https://accounts.spotify.com/authorize?" +
+            "client_id=YOURCLIENT&redirect_uri=https://www.example.com&response_type=code";
+
     public static void main(String[] args) {
-        List<String> mountains = new ArrayList<>(List.of("Sia", "Diplo", "Labrinth"));
-        List<String> runaway = new ArrayList<>(List.of("Lil peep"));
-        List<String> theGreatestShow = new ArrayList<>(List.of("Panic! At The Disco"));
-        List<String> allOutLife = new ArrayList<>(List.of("Slipknot"));
-        List<String> featured = new ArrayList<>(List.of("Mellow Morning", "Wake Up and Smell the Coffee"
-                , "Monday Motivation", "Songs to Sing in the Shower"));
-        List<String> mood = new ArrayList<>(List.of("Walk Like a Badass", "Rage Beats", "Arab Mood Booster"
-                , "Sunday Stroll"));
-        Scanner get = new Scanner(System.in);
-        boolean switch_control = true;
-        while (switch_control) {
-            String input = get.nextLine();
-            switch (input) {
-                case "new":
-                    System.out.println("---NEW RELEASES---");
-                    System.out.println("Mountains " + mountains);
-                    System.out.println("Runaway " + runaway);
-                    System.out.println("The Greatest Show " + theGreatestShow);
-                    System.out.println("All Out Life " + allOutLife);
-                    break;
-                case "featured":
-                    System.out.println("---FEATURED---");
-                    for (String st : featured) {
-                        System.out.println(st);
-                    }
-                    break;
-                case "categories":
-                    System.out.println("---CATEGORIES---");
-                    System.out.println("Top Lists\nPop\nMood\nLatin");
-                    break;
-                case "playlists Mood":
-                    System.out.println("---MOOD PLAYLISTS---");
-                    for (String st : mood) {
-                        System.out.println(st);
-                    }
-                    break;
-                case "exit":
-                    System.out.println("---GOODBYE!---");
-                    switch_control = false;
-                    break;
 
+        state.start();
+    }
 
-            }
+    public static void newOption() {
+        if (state.switch_control) {
+            System.out.println("---NEW RELEASES---");
+        } else {
+            System.out.println("Please, provide access for application.");
         }
 
     }
+
+    public static void featured() {
+        if (state.switch_control) {
+            System.out.println("---FEATURED---");
+        } else {
+            System.out.println("Please, provide access for application.");
+        }
+
+    }
+
+    public static void categories() {
+        if (state.switch_control) {
+            System.out.println("---CATEGORIES---");
+        } else {
+            System.out.println("Please, provide access for application.");
+        }
+
+    }
+
+    public static void playlists() {
+        if (state.switch_control) {
+            System.out.println("---MOOD PLAYLISTS---");
+        } else {
+            System.out.println("Please, provide access for application.");
+        }
+
+    }
+
+    public static void auth() {
+        state.switch_control = true;
+        System.out.println(url);
+        System.out.println("---SUCCESS---");
+
+    }
+
+
 }
---
+++ Music Advisor/task/src/advisor/state.java
@@ -0,0 +1,77 @@
+package advisor;
+
+import java.util.Scanner;
+
+//under production
+// Not used yet.
+
+public class state {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static State currentState = State.OPERATION_INPUT;
+    private static boolean isRunning = true;
+    public static boolean switch_control = false;
+
+    enum State {
+        OPERATION_INPUT, NEW, FEATURED, CATEGORIES, PLAYLISTS, AUTH
+    }
+
+    public static void start() {
+        while (isRunning) {
+            operationManager(currentState);
+        }
+    }
+
+    public static void operationManager(State currentS) {
+
+
+        switch (currentS) {
+            case OPERATION_INPUT:
+                String input = scanner.nextLine();
+                switch (input) {
+                    case "new":
+                        currentState = State.NEW;
+                        break;
+                    case "featured":
+                        currentState = State.FEATURED;
+                        break;
+                    case "categories":
+                        currentState = State.CATEGORIES;
+                        break;
+                    case "playlists Mood":
+                        currentState = State.PLAYLISTS;
+                        break;
+                    case "auth":
+                        currentState = State.AUTH;
+                        break;
+                    case "exit":
+                        System.out.println("---GOODBYE!---");
+                        isRunning = false;
+                }
+                break;
+            case NEW:
+                currentState = State.OPERATION_INPUT;
+                Main.newOption();
+                break;
+            case FEATURED:
+                currentState = State.OPERATION_INPUT;
+                Main.featured();
+                break;
+            case CATEGORIES:
+                currentState = State.OPERATION_INPUT;
+                Main.categories();
+                break;
+            case PLAYLISTS:
+                currentState = State.OPERATION_INPUT;
+                Main.playlists();
+                break;
+            case AUTH:
+                currentState = State.OPERATION_INPUT;
+                Main.auth();
+                break;
+
+        }
+
+    }
+
+
+}
